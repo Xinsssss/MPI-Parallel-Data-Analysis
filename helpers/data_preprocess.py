ef48f46ef27cb4b5bb3f3b3c9868cd40128b57e8
unknown <shenxinc@gmail.com>
2025-03-23 21:29:59 +1100

    Figuring out the structure of this program

---
+++ helpers/data_preprocess.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class Preprocessor():
+
+    def __init__(self):
+        return
+
+
+class Summariser():
+
+    # mode: user or hour
+    def __init__(self, mode):
+        return
+    
+    def sum_sentiment(self, data):
+        return
+        # return sentiment_rank
